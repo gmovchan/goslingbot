c5435899836bf9b59738cf7ba7d561e985f45f36
grigoryMovchan <grigory08@ya.ru>
2016-08-27 18:27:22 +0300

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('BOT_TOKEN', '260001713:AAHxq1ywj1lLTfs3BAv1Xp9Po3XxIUot2EY');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-define('HOSTING_URL', 'https://goslingbot.herokuapp.com/robot.php');
-define('WEBHOOK_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/setWebhook?url='.HOSTING_URL);
-//https://api.telegram.org/bot260001713:AAHxq1ywj1lLTfs3BAv1Xp9Po3XxIUot2EY/setWebhook?url=https://goslingbot.herokuapp.com/robot.php
-?>
